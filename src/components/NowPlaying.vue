1e69494fac7e1cd92daf5da455efa3f57469b5f4
mkymac25 <104637189+mkymac25@users.noreply.github.com>
2023-11-09 11:24:44 -0500

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -175,7 +175,7 @@ export default {
         trackArtists: 'paused',
         trackId: 'Puased',
         trackTitle: 'PAUSED'
-        this.playerData.trackTitle = trackTitle
+        this.player.trackTitle = trackTitle
       }
     },
 
